86e31088e00d902a93c74e7fdeb286412c88f66f
Hady Mohamed <hadymohamed@pursuit.org>
2021-05-17 21:45:33 -0400

    Added the Messages.js with random messages to be displayed when the user wins

---
+++ memory-game/src/Components/Messages.js
@@ -0,0 +1,12 @@
+export let messages = [
+  { title: "Well done", message: "How about another game?" },
+  { title: "Good Job", message: "I bet you can't do that again?" },
+  {
+    title: "Look At You!!",
+    message: "You think you can do it a bit faster next  time?",
+  },
+  {
+    title: "Way to go! You are great at this game!",
+    message: "That was super fast, Try Again?",
+  },
+];
